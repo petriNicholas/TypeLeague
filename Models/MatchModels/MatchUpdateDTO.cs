c41d8ef2e208adf970636def2db6fa9b87110663
Mikołaj Petri <petrimikolaj@gmail.com>
2023-09-30 18:34:51 +0200

    MatchModelDTO's

---
+++ Models/MatchModels/MatchUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TypeLeague.Models.MatchModels
+{
+    public class MatchUpdateDTO
+    {
+        public int Id { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+    }
+}
